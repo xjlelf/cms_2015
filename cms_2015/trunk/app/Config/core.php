c877b8624912a2de112602258306c7e3f0cdc397
2014-463 <MLS@2014-463deMacBook-Pro.local>
2015-02-15 11:40:06 +0800

    搭建环境

--- cms_2015/trunk/app/Config/core.php
+++ cms_2015/trunk/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'SHDdhsuhHDSUHDUAHshduahsduahdaieqloqz');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '12313675234789526489127561248124724157');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
